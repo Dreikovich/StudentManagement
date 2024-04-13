1eadc74b14dfe57aa26a2ec8296edb51848ce334
drewkevich <andrzejhk@gmail.com>
2024-04-14 00:35:01 +0200

    added generic form fields, tab for generation of the student credentials and visible password

--- frontend/student-management/src/pages/Forms/AddStudentForm/AddStudentForm.tsx
+++ frontend/student-management/src/pages/Forms/AddStudentForm/AddStudentForm.tsx
@@ -4,6 +4,7 @@ import {toast} from "react-toastify";
 import {useDispatch} from "react-redux";
 import {addStudent} from "../../../features/students/StudentSlice";
 import {v4 as uuidv4} from 'uuid';
+import FormField from "./FormFields";
 interface ModalProps {
     isOpen: boolean;
     closeModal: ()=>void;
@@ -14,6 +15,9 @@ const AddStudentForm: React.FC<ModalProps > = ({isOpen, closeModal}) => {
     const [email, setEmail] = useState<string>('');
     const [gender, setGender] = useState<string>('');
     const [status, setStatus] = useState<string>('')
+    const [isStudentInfoTab, setIsStudentInfoTab] = useState<boolean>(true);
+    const [login, setLogin] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     const dispatch = useDispatch();
 
@@ -41,15 +45,32 @@ const AddStudentForm: React.FC<ModalProps > = ({isOpen, closeModal}) => {
             });
     };
 
+
+
+    const handleGenerateCredentials = () => {
+        // Implement the logic to generate credentials here
+        if(email.length!==0){
+            setLogin(email);
+            setPassword('password');
+            return;
+        }
+
+        setPassword('password')
+
+
+
+
+    }
+
     return (
         <>
             {isOpen ?
                     (<div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" >
-                            <div className="flex items-center justify-center h-screen">
-                                <div className="relative  w-full max-w-lg p-8 mx-auto bg-white rounded-md shadow-lg">
-                                    <div className="mt-3 text-left">
-                                        <div className="flex justify-between items-center mb-6">
-                                            <h2 className="text-2xl font-semibold text-gray-800">Add Student</h2>
+                            <div className="flex items-center justify-center min-h-screen">
+                                <div  className=" w-full max-w-3xl h-fit p-8 mx-auto bg-white rounded-md shadow-lg">
+                                    <div className="mt-3 text-left relative">
+                                        <div className="flex  justify-between items-center mb-6">
+                                            <h2 className="text-2xl font-semibold text-gray-800">Add Student Form</h2>
                                             <button onClick={closeModal}
                                                     className="text-gray-600 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1.5 ml-auto inline-flex items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
@@ -58,83 +79,61 @@ const AddStudentForm: React.FC<ModalProps > = ({isOpen, closeModal}) => {
                                                 </svg>
                                             </button>
                                         </div>
-                                        <form onSubmit={handleSubmit}>
-                                            <div className="mb-4">
-                                                <label htmlFor="firstName"
-                                                       className="block text-gray-700 text-sm font-semibold mb-2">
-                                                    First Name
-                                                </label>
-                                                <input
-                                                    type="text"
-                                                    id="firstName"
-                                                    value={firstName}
-                                                    onChange={(e) => setFirstName(e.target.value)}
-                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                    required
-                                                />
+                                        <form onSubmit={handleSubmit} style={{ minHeight: '530px'}}>
+                                            <div className="tabs flex gap-5 mb-5">
+                                                <div  style={{
+                                                    textDecoration: isStudentInfoTab ? 'underline' : 'none',
+                                                    textDecorationThickness: isStudentInfoTab ? '2px' : 'initial',
+                                                    textUnderlineOffset: isStudentInfoTab ? '3px' : 'initial'
+                                                    }} className='StudentInfoTab'>
+                                                    <p className="text-2xl font-bold cursor-pointer" onClick={()=>setIsStudentInfoTab(true)}>Student Info</p>
+                                                </div>
+                                                <div style={{
+                                                    textDecoration: !isStudentInfoTab ? 'underline' : 'none',
+                                                    textDecorationThickness: !isStudentInfoTab ? '2px' : 'initial',
+                                                    textUnderlineOffset: !isStudentInfoTab ? '3px' : 'initial'
+                                                }}
+                                                     className="CredentialsTab">
+                                                    <p className="text-2xl font-bold cursor-pointer" onClick={()=>setIsStudentInfoTab(false)}>Generate Credentials</p>
+                                                </div>
+
                                             </div>
-                                            <div className="mb-4">
-                                                <label htmlFor="lastName" className="block text-gray-700 text-sm font-semibold mb-2">
-                                                    Last Name
-                                                </label>
-                                                <input
-                                                    type="text"
-                                                    id="lastName"
-                                                    value={lastName}
-                                                    onChange={(e) => setLastName(e.target.value)}
-                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                    required
-                                                />
-                                            </div>
-                                            <div className="mb-6">
-                                                <label htmlFor="email" className="block text-gray-700 text-sm font-semibold mb-2">
-                                                    Email
-                                                </label>
-                                                <input
-                                                    type="email"
-                                                    id="email"
-                                                    value={email}
-                                                    onChange={(e) => setEmail(e.target.value)}
-                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                    required
-                                                />
-                                            </div>
-                                            <div className="mb-6">
-                                                <label htmlFor="gender"
-                                                       className="block text-gray-700 text-sm font-semibold mb-2">
-                                                    Gender
-                                                </label>
-                                                <select
-                                                    id="gender"
-                                                    value={gender}
-                                                    onChange={(e) => setGender(e.target.value)}
-                                                    className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                    required
-                                                >
-                                                    <option value="">Select Gender</option>
-                                                    <option value="female">Female</option>
-                                                    <option value="male">Male</option>
-                                                    <option value="other">Other</option>
-                                                </select>
-                                            </div>
-                                            <div className="mb-6">
-                                                <label htmlFor="status"
-                                                       className="block text-gray-700 text-sm font-semibold mb-2">
-                                                    Status
-                                                </label>
-                                                <select
-                                                    id="status"
-                                                    value={status}
-                                                    onChange={(e) => setStatus(e.target.value)}
-                                                    className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                                    required
-                                                >
-                                                    <option value="">Select Status</option>
-                                                    <option value="active">Active</option>
-                                                    <option value="inactive">Inactive</option>
-                                                </select>
-                                            </div>
-                                            <div className="flex items-center justify-between">
+
+                                            {isStudentInfoTab ?
+                                                <>
+                                                    <FormField label="First Name" id="firstName" type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} required={true}/>
+                                                    <FormField label="Last Name" id="lastName" type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} required={true}/>
+                                                    <FormField label="Email" id="email" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required={true}/>
+                                                    <FormField label="Gender" id="gender" type="select" value={gender} onChange={(e) => setGender(e.target.value)}
+                                                    options={[
+                                                        { value: 'Select Gender', "label":"Select Gender"},
+                                                        { value: 'Male', label:"male"},
+                                                        { value: 'Female', label:"female"}
+
+                                                    ]}
+                                                    />
+                                                    <FormField label="Status" id="status" type="select" value={status} onChange={(e) => setStatus(e.target.value)}
+                                                    options={[
+                                                        { value: 'Select Status', "label":"Select Status"},
+                                                        { value: 'active', label:"Active"},
+                                                        { value: 'inactive', label:"Inactive"}]}
+                                                    />
+
+                                                </>:
+                                                <>
+                                                    <FormField label="Login" id="login" type="text" value={login} onChange={(e) => setLogin(e.target.value)} required={true}/>
+                                                    <FormField label="Password" id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} required={true}/>
+
+                                                </>
+                                            }
+                                            <div
+                                                className="flex items-center justify-between absolute bottom-0 right-0 mb-0">
+                                                {!isStudentInfoTab ?
+                                                    <button type="button" onClick={handleGenerateCredentials}
+                                                            className=" mr-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                                        Generate Credentials
+                                                    </button> :null
+                                                }
                                                 <button
                                                     type="submit"
                                                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
@@ -145,9 +144,9 @@ const AddStudentForm: React.FC<ModalProps > = ({isOpen, closeModal}) => {
                                         </form>
                                     </div>
                                 </div>
-                         </div>
+                            </div>
                     </div>)
-            :null}
+                : null}
 
         </>
 
---
+++ frontend/student-management/src/pages/Forms/AddStudentForm/FormFields.tsx
@@ -0,0 +1,79 @@
+import React, {ChangeEvent, useState} from 'react';
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+
+interface FormFieldProps {
+    label: string;
+    id: string;
+    type: 'text' | 'password' | 'select';
+    value: string;
+    onChange: (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => void;
+    options?: { value: string; label: string }[];
+    required?: boolean;
+}
+
+const FormField: React.FC<FormFieldProps> = ({
+                                                 label,
+                                                 id,
+                                                 type,
+                                                 value,
+                                                 onChange,
+                                                 options,
+                                                 required = false,
+                                             }) => {
+    const [passwordVisible, setPasswordVisible] = useState(true);
+    const togglePasswordVisibility = () => {
+        setPasswordVisible(!passwordVisible);
+    };
+
+    return (
+        <div className="mb-4">
+            <label htmlFor={id}
+                   className="block text-gray-700 text-sm font-semibold mb-2">
+                {label}
+            </label>
+            {type === 'select' ? (
+                <select
+                    id={id}
+                    value={value}
+                    onChange={onChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    required={required}>
+                    {options?.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            ) : type ==='password'?(
+                <div className="relative ">
+                    <input
+                        type={passwordVisible?'text':'password'}
+                        id={id}
+                        value={value}
+                        onChange={onChange}
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        required={required}/>
+                    <button
+                        onClick={togglePasswordVisibility}
+                        type="button"
+                        className="absolute  right-0  top-3 pr-3 flex items-center text-sm "
+                    >
+                        <FontAwesomeIcon icon={passwordVisible ?  faEye:faEyeSlash } />
+                    </button>
+                </div>) : (
+                <input
+                    type={type}
+                    id={id}
+                    value={value}
+                    onChange={onChange}
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                    required={required}/>)
+            }
+
+        </div>
+    );
+};
+
+export default FormField;
