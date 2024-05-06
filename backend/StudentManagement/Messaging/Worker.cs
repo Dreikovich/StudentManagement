3e65f60349c81018828177a89a342658e12a4ffc
drewkevich <andrzejhk@gmail.com>
2024-05-06 17:35:27 +0200

    add worker and docker compose file which set the rabbit mq

---
+++ backend/StudentManagement/Messaging/Worker.cs
@@ -0,0 +1,69 @@
+using System.Text;
+using Messaging.Configuration;
+using Messaging.Services;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Messaging;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly IModel _channel;
+    private readonly string _queueName;
+    private readonly RabbitMqPublisher _publisher;
+
+    public Worker(RabbitMqConnectionService connectionService, RabbitMqPublisher publisher, ILogger<Worker> logger)
+    {
+        _logger = logger;
+        _channel = connectionService.CreateModel();
+        _publisher = publisher;
+        _queueName = connectionService.GetQueueName();
+
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var message = "Hello";
+        Console.WriteLine($"Received: {message}");
+        try
+        {
+            _logger.LogInformation("Processing message...");
+            _publisher.Publish($"Message '{message}' processed successfully.");
+            _logger.LogInformation("Message processed successfully.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error processing message.");
+        }
+        
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (ch, ea) =>
+        {
+            var content = Encoding.UTF8.GetString(ea.Body.ToArray());
+            _logger.LogInformation($"Received message: {content}");
+            
+            Task.Delay(TimeSpan.FromMinutes(2)).ContinueWith(t =>
+            {
+                _logger.LogInformation($"Processed message: {content}");
+                _channel.BasicAck(ea.DeliveryTag, false);
+            });
+        };
+
+        _channel.BasicConsume(_queueName, false, consumer);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10), stoppingToken);
+        }
+    }
+    
+    public override void Dispose()
+    {
+        _channel?.Close();
+        base.Dispose();
+    }
+}
