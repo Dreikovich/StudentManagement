0d0cf24e51513ece6a518c0a9f235dbde2da6c3b
drewkevich <andrzejhk@gmail.com>
2024-05-20 12:25:23 +0200

    added missing files related to NotificationService project

---
+++ backend/StudentManagement/NotificationService/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.Extensions.Options;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSignalR(options =>
+{
+    options.EnableDetailedErrors = true;
+    options.KeepAliveInterval = TimeSpan.FromSeconds(30);
+    options.HandshakeTimeout = TimeSpan.FromSeconds(15);
+});
+   
+    
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseCors(x => x
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(origin => true) // allow any origin
+        .AllowCredentials()); // allow credentials
+}
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+app.MapHub<NotificationHub>("/notificationHub");
+
+app.Run();
+
